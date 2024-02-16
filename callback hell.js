3d7354a37de90eb28009f3a914905de57b62414e
OmeGa8989 <115368410+OmeGa8989@users.noreply.github.com>
2024-02-16 23:33:50 +0530

    Update callback hell.js

--- callback hell.js
+++ callback hell.js
@@ -9,6 +9,7 @@ const delayedColorChange = (newColor, delay, doNext) => {
 delayedColorChange('red', 1000, () => {
     delayedColorChange('orange', 1000, () => {
         delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('teal', 1000, () => {
             delayedColorChange('green', 1000, () => {
                 delayedColorChange('blue', 1000, () => {
 
