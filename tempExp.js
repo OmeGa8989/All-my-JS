984e38a1f97615c83c9a91c2a16dc5ca0a8828e7
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-04-20 01:00:29 +0530

    added a random number routing

--- tempExp.js
+++ tempExp.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
+// let num = Math.random() * 10 + 1;
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -12,5 +13,10 @@ app.listen(8080, () => {
 
 
 app.get('/', (req, res) => {
-    res.render('home1.ejs')
+    res.render('home.ejs')
+})
+
+app.get('/rand', (req, res) => {
+    const num = (Math.floor(Math.random() * 10) + 1);
+    res.render('home.ejs', { num })
 })
