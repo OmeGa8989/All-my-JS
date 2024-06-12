d2d4ab61132787ef58d1d9efc46ee97cca6dddbf
Adhyan Aditya <115368410+OmeGa8989@users.noreply.github.com>
2024-06-13 02:20:11 +0530

    Update twoDLinearSearch.java
    add max min functions . showed how to return a 2-D array

--- twoDLinearSearch.java
+++ twoDLinearSearch.java
@@ -10,10 +10,17 @@ public class twoDLinearSearch {
                 { 95, 22, 99, 88 }
         };
         int target = 22;
+        String t = target + "";
+        if (t.length() % 2 == 0) {
+            System.out.println("Its a an even Number");
+        }
 
         int[] result = search(arr, target);
         System.out.println(Arrays.toString(result));
-        // System.out.println(result);
+        // System.out.println(result); //garbage value
+
+        System.out.println("Smallest Element in the array:" + minValue(arr));
+        System.out.println("Largest Element in the array:" + maxValue(arr));
     }
 
     static int[] search(int[][] arr, int target) {
@@ -29,5 +36,31 @@ public class twoDLinearSearch {
     // new int (new object is written here because the array isn't initialised in
     // this function so
     // we need to declare an object to return the positons)
+
+    static int minValue(int[][] arr) {
+        int min = arr[0][0];
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                if (arr[row][column] < min) {
+                    min = arr[row][column];
+                }
+            }
+        }
+        return min;
+
+    }
+
+    static int maxValue(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                if (arr[row][column] > max) {
+                    max = arr[row][column];
+                }
+            }
+        }
+        return max;
+
+    }
 
 }
